fc7ae0202fc7844478d0ceb22edad20e7e53ac78
Axel Román <axelroman20@gmail.com>
2022-09-09 14:38:14 -0500

    add: package helmet and favicon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { HeaderPage } from './page/headerPage';
 import { AboutPage } from './page/aboutPage';
 import { NavbarWidget } from './widget/navbar';
-
-
+import { Helmet } from "react-helmet";
 
 import './css/App.css';
+import favicon from './assets/favicon.ico';
 
 function App() {
   return (
     <div className="App">
+
+      <Helmet>
+        <html lang="en" />
+        <title>Axel Roman</title>
+        <meta name="theme-color" content="#E6E6FA" />
+        <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+      </Helmet>
+
       <NavbarWidget />
       <HeaderPage />
       <AboutPage />
